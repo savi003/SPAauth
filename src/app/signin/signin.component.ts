69afa6cff77401c4eb5518a135f5ccbcf6a50a53
savi003 <thomasavio003@gmail.com>
2022-10-15 16:02:17 +0530

    sigincomponent

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  SigninForm!:FormGroup
+  constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
+    this.SigninForm=this.formbuilder.group({
+      email:[''],
+      password:['']
+    })
+  }
+  signin(){
+    this.http.get<any>('http://localhost:3000/user').subscribe(
+      res=>{
+        const user=res.find((a:any)=>{
+          return a.email===this.SigninForm.value.email&&a.password===this.SigninForm.value.password
+        })
+        if(user){
+          
+          // alert("Login Succes")
+          this.SigninForm.reset;
+        }
+        else{
+          alert("user not found")
+        }
+      }
+      );
   }
 
 }
